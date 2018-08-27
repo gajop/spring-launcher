7671b1700ffb98c56f0031896d308e9e702f6f4f
Gajo Petrovic <gajo01@gmail.com>
2018-08-28 01:13:42 +0900

    fix pr-downloader path and progress

--- src/spring_platform.js
+++ src/spring_platform.js
@@ -1,12 +1,17 @@
 const log = require('electron-log');
+const path = require('path');
+const fs = require('fs');
 
 const platformName = process.platform
 
 if (platformName === "win32") {
-  exports.prDownloaderPath = "./bin/pr-downloader.exe";
+  exports.prDownloaderPath = path.resolve(`${__dirname}/../bin/pr-downloader.exe`);
   exports.springBin = "spring.exe";
 } else if (platformName === "linux") {
-  exports.prDownloaderPath = "./bin/pr-downloader";
+  exports.prDownloaderPath = path.resolve(`${__dirname}/../bin/pr-downloader`);
+  if (!fs.existsSync(exports.prDownloaderPath)) {
+    exports.prDownloaderPath = path.resolve(`${__dirname}/../../../bin/pr-downloader`);
+  }
   exports.springBin = "spring";
 } else {
   log.error(`Unsupported platform: ${platformName}`);
