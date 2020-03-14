9b2a68253446770842f0d4293ae8e1985fb483dc
Gajo Petrovic <gajopetrovic@gmail.com>
2020-03-14 17:19:13 +0900

    nlc

--- src/exts/open_file.js
+++ src/exts/open_file.js
@@ -1,9 +1,7 @@
-// const { exec } = require('child_process');
 const { bridge } = require('../spring_api');
 const file_opener = require('../file_opener');
 
 bridge.on("OpenFile", (command) => {
-  // const fullPath = file_opener.GetOpenCommand(command.path);
   if (file_opener.open(command.path)) {
     bridge.send("OpenFileFinished", {
       path: command.path
@@ -13,21 +11,4 @@ bridge.on("OpenFile", (command) => {
       path: command.path
     });
   }
-
-  // exec(fullPath, (err, stdout, stderr) => {
-  //   if (err) {
-  //     bridge.send("OpenedFileFailed", {
-  //       path: command.path,
-  //       stderr: stderr,
-  //       stdout: stdout,
-  //     });
-  //   } else {
-  //     bridge.send("OpenFileFinished", {
-  //       path: command.path,
-  //       stderr: stderr,
-  //       stdout: stdout,
-  //     });
-  //   }
-  // });
-  // console.log(fullPath);
 });
