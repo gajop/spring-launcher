c4699cfc862209f467258662a1c722041d900913
Gajo Petrovic <gajopetrovic@gmail.com>
2021-02-23 19:21:37 +0900

    rate limit the map parser

--- src/exts/map_parser.js
+++ src/exts/map_parser.js
@@ -5,15 +5,26 @@ const { MapParser } = require('spring-map-parser');
 const { bridge } = require('../spring_api');
 const springPlatform = require('../spring_platform');
 
+let concurrentCalls = 0;
+
 bridge.on('ParseMiniMap', async command => {
 	const destinationPath = path.join(springPlatform.writePath, command.destination);
-	const parser = new MapParser({ verbose: true, mipmapSize: 4, skipSmt: true });
+	const miniMapSize = command.miniMapSize || 4;
+	const parser = new MapParser({ verbose: true, mipmapSize: miniMapSize, skipSmt: true });
 	const map = await parser.parseMap(command.mapPath);
 
-	await map.miniMap.toFile(destinationPath);
+	while (concurrentCalls > 0) {
+		await new Promise(resolve => setTimeout(resolve, 1000));
+	}
+	try {
+		concurrentCalls++;
+		await map.miniMap.toFile(destinationPath);
+	} finally {
+		concurrentCalls--;
+	}
 
 	bridge.send('ParseMiniMapFinished', {
 		mapPath : command.mapPath,
-		destinationPath: command.destinationPath
+		destinationPath: destinationPath
 	});
 });
