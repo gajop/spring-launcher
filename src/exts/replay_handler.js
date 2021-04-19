aed0840f06e4aa414c5653320fd133135d7b2867
Gajo Petrovic <gajopetrovic@gmail.com>
2021-04-20 00:07:03 +0900

    try catch stuff

--- src/exts/replay_handler.js
+++ src/exts/replay_handler.js
@@ -4,20 +4,25 @@ const { DemoParser } = require('sdfz-demo-parser');
 
 const { bridge } = require('../spring_api');
 const springPlatform = require('../spring_platform');
+const { log } = require('../spring_log');
 
 bridge.on('ReadReplayInfo', async command => {
-	const demoPath = path.join(springPlatform.writePath, command.relativePath);
-	const parser = new DemoParser();
-	const demo = await parser.parseDemo(demoPath);
+	try {
+		const demoPath = path.join(springPlatform.writePath, command.relativePath);
+		const parser = new DemoParser();
+		const demo = await parser.parseDemo(demoPath);
 
-	const info = {
-		relativePath : command.relativePath,
-		engine: demo.header.versionString,
-		game: demo.info.hostSettings.gametype,
-		map: demo.info.hostSettings.mapname,
-		players: demo.info.players,
-		gameTime: demo.header.gameTime
-	};
+		const info = {
+			relativePath : command.relativePath,
+			engine: demo.header.versionString,
+			game: demo.info.hostSettings.gametype,
+			map: demo.info.hostSettings.mapname,
+			players: demo.info.players,
+			gameTime: demo.header.gameTime
+		};
 
-	bridge.send('ReplayInfo', info);
+		bridge.send('ReplayInfo', info);
+	} catch (err) {
+		log.error(`Error parsing file: ${command.relativePath}`);
+	}
 });
