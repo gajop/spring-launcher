e37ce99c0c00f2f76bdfea5acbbb09ea0b1d3a34
Marek Rusinowski <marekrusinowski@gmail.com>
2022-08-16 23:56:45 +0200

    Preserve file structure when modifying springsettings

--- src/springsettings.js
+++ src/springsettings.js
@@ -7,23 +7,24 @@ const os = require('os');
 const { writePath } = require('./spring_platform');
 
 class Springsettings extends EventEmitter {
-	// Parsing according to implementation in rts/System/Config/ConfigSource.cpp
-	readSettings(springsettingsPath) {
-		var fileContent = '';
+
+	// Returns instance of Map, each config line in it's own entry.
+	// Map preserves insertion order, so it can be iterated in the same order as
+	// in the file, and it also contains comments, under unique
+	// Symbol('comment') keys.
+	#readSettings(springsettingsPath) {
+		let fileContent = '';
 		try {
 			fileContent = fs.readFileSync(springsettingsPath).toString();
 		} catch (err) {
 			// ignore errors
 		}
 		const lines = fileContent.split(/\r?\n/g);
-		if (lines[lines.length-1] === '') lines.pop();
-		const values = {};
-		const comments = {};
-		let comment = '';
+		const settings = new Map();
 		for (let i = 0; i < lines.length; i++) {
 			const line = lines[i];
 			if (line.trim() === '' || line.trim()[0] === '#') {
-				comment += line + '\n';
+				settings.set(Symbol('comment'), line);
 				continue;
 			}
 			const keyvalue = line.split(/=/, 2);
@@ -32,56 +33,50 @@ class Springsettings extends EventEmitter {
 					`Error loading ${springsettingsPath}: Cannot parse line ` +
 					`${i+1} ("${line}"): it's not a comment or option assignment`);
 			}
-			const key = keyvalue[0].trim();
-			const value = keyvalue[1].trim();
-
-			values[key] = value;
-			comments[key] = comment;
-			comment = '';
+			settings.set(keyvalue[0].trim(), keyvalue[1].trim());
 		}
-		return {
-			values: values,
-			comments: comments,
-			endComment: comment,
-		};
+		return settings;
 	}
 
-	writeSettings(settings, springsettingsPath) {
-		const valuesKeys = Object.keys(settings.values).sort();
-		const commentKeys = Object.keys(settings.comments).sort();
+	#writeSettings(settings, springsettingsPath) {
 		const result = [];
-		let commi = 0;
-		for (let vali = 0; vali < valuesKeys.length; vali++) {
-			const valk = valuesKeys[vali];
-			for (; commi < commentKeys.length && commentKeys[commi] <= valk; commi++) {
-				result.push(settings.comments[commentKeys[commi]]);
+		for (const [key, value] of settings) {
+			if (typeof key === 'symbol' && key.description === 'comment') {
+				result.push(value);
+			} else if (typeof key === 'string') {
+				result.push(`${key} = ${value}`);
+			} else {
+				throw new Error(`internal error: unexpected key in map: ${key}`);
 			}
-			result.push(valk + ' = ' + settings.values[valk] + '\n');
 		}
-		for (; commi < commentKeys.length; commi++) {
-			result.push(settings.comments[commentKeys[commi]]);
+		fs.writeFileSync(springsettingsPath, result.join(os.EOL));
+	}
+
+	#applyDefaults(settings, defaults) {
+		for (const key in defaults) {
+			if (!settings.has(key)) {
+				settings.set(key, defaults[key]);
+			}
 		}
-		result.push(settings.endComment);
-		fs.writeFileSync(springsettingsPath, result.join(''));
+	}
+
+	#applyOverrides(settings, overrides) {
+		for (const key in overrides) {
+			if (overrides[key] === null) {
+				settings.delete(key);
+			} else {
+				settings.set(key, overrides[key]);
+			}
+		}
 	}
 
 	applyDefaultsAndOverrides(overrides) {
 		const defaults = require('./springsettings.json');
 		const springsettingsPath = `${writePath}/springsettings.cfg`;
-		const settings = this.readSettings(springsettingsPath);
-		for (const key in defaults) {
-			if (!(key in settings.values)) {
-				settings.values[key] = defaults[key];
-			}
-		}
-		for (const key in overrides) {
-			if (overrides[key] === null) {
-				delete settings.values[key];
-			} else {
-				settings.values[key] = overrides[key];
-			}
-		}
-		this.writeSettings(settings, springsettingsPath);
+		const settings = this.#readSettings(springsettingsPath);
+		this.#applyDefaults(settings, defaults);
+		this.#applyOverrides(settings, overrides);
+		this.#writeSettings(settings, springsettingsPath);
 	}
 }
 
