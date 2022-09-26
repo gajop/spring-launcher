gajop/spring-launcher

commit fc5dccfd63b19661f0fb76c89534326bb3e2cf26

Update dependencies

src/launcher_wizard_util.js
@@ -8,7 +8,7 @@ const { gui } = require('./launcher_gui');
 
 function generateAndBroadcastWizard()
 {
-	const checkForUpdates = settings.get('checkForUpdates', null);
+	const checkForUpdates = settings.getSync('checkForUpdates', null);
 	if (checkForUpdates !== null) {
 		config.no_downloads = !checkForUpdates;
 	}

src/launcher_gui.js
@@ -113,7 +113,7 @@ app.prependListener('ready', () => {
 	// Workaround for linux/wayland on which electron has a problem with
 	// properly setting window size from the beggining and a simple size refresh
 	// after it got rendered once fixes it.
-	// TODO: check if it can be dropped once on electron >= 18.
+	// TODO: check if it can be dropped once on electron >= 21.
 	mainWindow.once('show', () => {
 		setTimeout(() => {
 			mainWindow.setSize(width, height);

src/main.js
@@ -88,14 +88,14 @@ function maybeSetConfig(cfgName) {
 		return false;
 	}
 
-	settings.set('config', cfgName);
+	settings.setSync('config', cfgName);
 	generateAndBroadcastWizard();
 
 	return true;
 }
 
 ipcMain.on('change-cfg', (_, cfgName) => {
-	settings.set('checkForUpdates', undefined);
+	settings.setSync('checkForUpdates', undefined);
 	if (maybeSetConfig(cfgName)) {
 		wizard.setEnabled(true);
 	}
@@ -118,11 +118,11 @@ ipcMain.on('wizard-next', () => {
 });
 
 ipcMain.on('wizard-check-for-updates', (_, checkForUpdates) => {
-	if (checkForUpdates === settings.get('checkForUpdates')) {
+	if (checkForUpdates === settings.getSync('checkForUpdates')) {
 		return;
 	}
 	log.info('wizard-check-for-updates', checkForUpdates);
-	settings.set('checkForUpdates', checkForUpdates);
+	settings.setSync('checkForUpdates', checkForUpdates);
 	generateAndBroadcastWizard();
 });
 
@@ -131,12 +131,16 @@ app.on('ready', () => {
 		return;
 	}
 	// Use local settings file
-	settings.setPath(`${writePath}/launcher_cfg.json`);
-	const oldConfig = settings.get('config');
+	settings.configure({
+		dir: writePath,
+		fileName: 'launcher_cfg.json',
+		prettify: true
+	});
+	const oldConfig = settings.getSync('config');
 	if (oldConfig) {
 		if (!maybeSetConfig(oldConfig)) {
 			// forget invalid configs
-			settings.set('config', undefined);
+			settings.unsetSync('config');
 		}
 	}
 });

commit b4659424c0b4befd415dfb6e8eb3d9043f0c902f

Fix selection of checkForUpdates when option is not explicitly set.
This fixes regression introduced in fc5dccfd6, as API changed
without indication in the library documentation.

src/launcher_wizard_util.js
@@ -8,9 +8,8 @@ const { gui } = require('./launcher_gui');
 
 function generateAndBroadcastWizard()
 {
-	const checkForUpdates = settings.getSync('checkForUpdates', null);
-	if (checkForUpdates !== null) {
-		config.no_downloads = !checkForUpdates;
+	if (settings.hasSync('checkForUpdates')) {
+		config.no_downloads = !settings.getSync('checkForUpdates');
 	}
 
 	wizard.generateSteps();